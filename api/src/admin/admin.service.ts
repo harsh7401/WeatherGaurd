5ddaa988514e60067bfdb4a3cb3dd95daef49d29
Harsh Shrivastava <harshcse1075@gmail.com>
2026-06-29 22:21:23 +0530

    updated admin.module.ts

--- api/src/admin/admin.service.ts
+++ api/src/admin/admin.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
 
+import { UsersService } from '../users/users.service';
+
 @Injectable()
-export class AdminService {}
+export class AdminService {
+  constructor(
+    private readonly usersService: UsersService,
+  ) {}
+
+  async getPendingUsers() {
+    return this.usersService.findPendingUsers();
+  }
+
+  async getAllUsers() {
+    return this.usersService.findAllUsers();
+  }
+
+  async approveUser(id: string) {
+    return this.usersService.approveUser(id);
+  }
+
+  async rejectUser(id: string) {
+    return this.usersService.rejectUser(id);
+  }
+}

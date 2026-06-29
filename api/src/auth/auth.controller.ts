eec8483da8941a52f4ebac5e33272d24584aae26
Harsh Shrivastava <harshcse1075@gmail.com>
2026-06-29 22:09:37 +0530

    auth.controller.ts updated

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,8 +1,16 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 
 import { AuthService } from './auth.service';
+
 import { GoogleAuthGuard } from './guards/google-auth.guard';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+
+import { CurrentUser } from '../decorators/current-user.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -10,21 +18,24 @@ export class AuthController {
     private readonly authService: AuthService,
   ) {}
 
+  // Login with Google
   @Get('google')
   @UseGuards(GoogleAuthGuard)
-  async googleAuth() {
+  async googleAuth(): Promise<void> {
     // Passport redirects to Google
   }
 
+  // Google Callback
   @Get('google/callback')
   @UseGuards(GoogleAuthGuard)
   async googleCallback(@Req() req: any) {
     return this.authService.login(req.user);
   }
 
+  // Get Current Logged-in User
   @Get('me')
   @UseGuards(JwtAuthGuard)
-  getProfile(@Req() req: any) {
-    return req.user;
+  getProfile(@CurrentUser() user: any) {
+    return user;
   }
 }

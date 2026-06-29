d7d66e14c58b843ba40fcdefe05bf82c2645f539
Harsh Shrivastava <harshcse1075@gmail.com>
2026-06-29 15:44:50 +0530

    adding new file

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,13 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AdminModule } from './admin/admin.module';
 
 @Module({
   imports: [
+    // Load environment variables globally
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // Connect to MongoDB
+    MongooseModule.forRoot(process.env.MONGO_URI as string),
+
+    // Feature Modules
     AuthModule,
     UsersModule,
     AdminModule,
   ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

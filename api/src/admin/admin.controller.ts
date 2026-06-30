c949e247b33e16a31c50b09bbde8bf896b8134bb
Harsh Shrivastava <harshcse1075@gmail.com>
2026-06-30 17:29:19 +0530

    update Update api/src/admin/admin.controller.ts

--- api/src/admin/admin.controller.ts
+++ api/src/admin/admin.controller.ts
@@ -9,15 +9,21 @@ import {
 import { AdminService } from './admin.service';
 
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from '../decorators/roles.decorator';
 
 @Controller('admin')
-@UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Roles('ADMIN')
 export class AdminController {
   constructor(
     private readonly adminService: AdminService,
   ) {}
+
+  @Get('dashboard')
+  getDashboardStats() {
+    return this.adminService.getDashboardStats();
+  }
 
   @Get('pending-users')
   getPendingUsers() {

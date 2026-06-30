919cf7a618afb34499b2c19afe7b8036ae1d658b
Harsh Shrivastava <harshcse1075@gmail.com>
2026-06-30 15:02:06 +0530

    update App Routes

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,11 +1,7 @@
+import AppRoutes from "./routes/AppRoutes";
+
 function App() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">
-        WeatherGuard Admin
-      </h1>
-    </div>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
